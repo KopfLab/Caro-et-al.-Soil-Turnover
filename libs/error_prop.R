c42c8306e14f93f2f6970ae2669fe8fe8dd3faee
tacaro <tristan.caro@colorado.edu>
2022-03-15 22:14:33 -0600

    uncertainty propagation

---
+++ libs/error_prop.R
@@ -0,0 +1,30 @@
+#' Propagation of uncertainty
+#' @param a assimilation efficiency
+#' @param F_L 2F of label 
+#' @param F_T 2F of biomass at time t
+#' @param F_0 2F of biomass at time 0
+#' @param t incubation time
+#' @param sF_0 uncertainty in F_0
+#' @param sF_L uncertainty in F_L
+#' @param sF_T uncertainty in F_T
+#' @param sa uncertainty in a
+#' @return the uncertainty in µ, turnover rate 'su'
+
+sigma_mu <- function(a, F_L, F_T, F_0, t, sF_0, sF_L, sF_T, sa) {
+  
+  # the big equation :)
+  
+  # numerator
+  num = sqrt(
+    ((a*F_L - F_T)^2 * sF_0^2) +
+    ((a*F_L - F_0)^2 * sF_T^2) +
+    (a^2 * (F_0 - F_T)^2 * sF_L^2) +
+    (F_L^2 * (F_0 - F_T)^2 * sa^2)
+  )
+  
+  # denominator
+  denom = t * (a*F_L - F_0) * (a*F_L - F_T)
+  
+  su = num/denom
+  return(su)
+}
