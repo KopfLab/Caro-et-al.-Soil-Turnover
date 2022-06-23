60e1a95917cc5c0c9d06fc4060ecde76f7d1a7bb
tacaro <tristan.caro@colorado.edu>
2022-06-23 16:00:10 -0600

    updated FA names

--- libs/rename_FAs.R
+++ libs/rename_FAs.R
@@ -8,9 +8,10 @@ rename_FAs <- function(df) {
       mutate(compound = case_when(
         compound == "16:1 cis 9" ~ "16:1ω7c",
         compound == "16:1 trans 9" ~ "16:1:ω5c",
-        compound == "18:2 9,12" ~ "18:2",
-        compound == "18:1 cis 9" ~ "18:1ω9",
-        compound == "18:1 trans 9" ~ "18:1ω7",
+        compound == "18:2 9, 12" ~ "18:2",
+        compound == "18:1 cis 9" ~ "18:1ω9c",
+        compound == "18:1 trans 9" ~ "18:1ω7c",
+        compound == "17:0 cyclo" ~ "cyclo 17:0",
         TRUE ~ compound
       ))
   )
